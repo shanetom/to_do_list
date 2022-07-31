a40e1e0f65c1d95e48b9633b3779886632ce7f7b
Mackhaster <mrshanelyse@hotmail.com>
2022-07-31 10:36:14 -0700

    Add new methods to script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
 const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form');
+const newListInput = document.querySelector('[data-new-list-input');
 
-let lists = [{
-    id: 1,
-    name: 'name'
-},{
-    id: 2,
-    name: 'todo'
-}];
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'; 
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY) || []);
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const listName = newListInput.value;
+    if (listName == null || listName === '') return;
+    const list = createList(listName);
+    newListInput.value = null;
+    lists.push(list);
+    render();
+})
+
+function createList(name) {
+    return { id: Date.now().toString(), name: name, tasks: [] };
+}
+
+function saveAndRender() {
+    save();
+    render();
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+}
 
 function render() {
     clearElement(listsContainer);
